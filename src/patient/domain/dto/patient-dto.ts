aff63f5c728af294b85fc80711fdb576ca35bebc
tomascavataio <tomascavataio@gmail.com>
2020-07-29 13:06:14 +0200

    Swagger documentation details

--- src/patient/domain/dto/patient-dto.ts
+++ src/patient/domain/dto/patient-dto.ts
@@ -1,16 +1,35 @@
 import { UserType, Gender, Address } from 'src/common/interfaces/user-types.interface';
 import { Insurance } from 'src/patient/domain/models/patient.interface';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class PatientDto {
     _id: string;
+    @ApiProperty({ enum: ['Patient', 'Professional'] })
     userType: UserType;
+    @ApiProperty()
     name: string;
+    @ApiProperty()
     firstSurname: string;
+    @ApiProperty()
     secondSurname: string;
+    @ApiProperty({ enum: ['Male', 'Female', 'Other'] })
     gender: Gender;
+    @ApiProperty()
     birthdate: Date;
+    @ApiProperty()
     nif: string;
+    @ApiProperty({
+        properties: {
+            city: { type: 'string' },
+            street: { type: 'string' },
+            streetNumber: { type: 'string' },
+            doorNumber: { type: 'string' },
+            postalCode: { type: 'string' }
+        }
+    })
     address: Address;
+    @ApiProperty()
     nhc: string;
+    @ApiProperty()
     insuranceCompanies: Insurance[];
 }
--- src/professional/domain/dto/professional-dto.ts
+++ src/professional/domain/dto/professional-dto.ts
@@ -1,16 +1,36 @@
 import { UserType, Gender, Address } from 'src/common/interfaces/user-types.interface'
 import { Professions } from 'src/professional/domain/models/professional.interface'
+import { ApiProperty } from '@nestjs/swagger';
 
 export class ProfessionalDto {
+    @ApiProperty()
     _id: string;
+    @ApiProperty({ enum: ['Patient', 'Professional'] })
     userType: UserType;
+    @ApiProperty()
     name: string;
+    @ApiProperty()
     firstSurname: string;
+    @ApiProperty()
     secondSurname: string;
+    @ApiProperty({ enum: ['Male', 'Female', 'Other'] })
     gender: Gender;
+    @ApiProperty()
     birthdate: Date;
+    @ApiProperty()
     nif: string;
+    @ApiProperty({
+        properties: {
+            city: { type: 'string' },
+            street: { type: 'string' },
+            streetNumber: { type: 'string' },
+            doorNumber: { type: 'string' },
+            postalCode: { type: 'string' }
+        }
+    })
     address: Address;
+    @ApiProperty()
     medicalBoardNumber: string;
+    @ApiProperty()
     professionalType: Professions;
 }
